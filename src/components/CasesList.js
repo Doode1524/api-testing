47ad1b98c21d7215634399b6f5bdaabd00c948dc
joey <doode1524@gmail.com>
2021-05-27 16:58:57 -0400

    added colors

--- src/components/CasesList.js
+++ src/components/CasesList.js
@@ -10,8 +10,10 @@ import Paper from "@material-ui/core/Paper";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
+    backgroundColor: "#B2B2B2",
+    color: theme.palette.common.black,
+    fontSize: 22,
+    fontWeight: 900,
   },
   body: {
     fontSize: 14,
@@ -20,8 +22,9 @@ const StyledTableCell = withStyles((theme) => ({
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
+    backgroundColor: "#E9E3E6",
     "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
+      backgroundColor: "#E9E3E9",
     },
   },
 }))(TableRow);
@@ -33,57 +36,62 @@ const useStyles = makeStyles({
 });
 
 const CasesList = (props) => {
-    const classes = useStyles();
-    return (
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>Country</StyledTableCell>
-                <StyledTableCell align="right">Confirmed</StyledTableCell>
-                <StyledTableCell align="right">Deaths</StyledTableCell>
-                <StyledTableCell align="right">Population</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {props.cases.map((data) => (
-                <StyledTableRow key={Object.keys(data)[0]}>
-                  <StyledTableCell component="th" scope="row">
-                  {Object.keys(data)[0]}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">{data[Object.keys(data)].confirmed}</StyledTableCell>
-                  <StyledTableCell align="right">{data[Object.keys(data)].deaths}</StyledTableCell>
-                  <StyledTableCell align="right">{data[Object.keys(data)].population}</StyledTableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      );
+  const classes = useStyles();
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="sticky table" stickyHeader>
+        <TableHead stickyHeader>
+          <TableRow>
+            <StyledTableCell>Country</StyledTableCell>
+            <StyledTableCell align="right">Confirmed</StyledTableCell>
+            <StyledTableCell align="right">Deaths</StyledTableCell>
+            <StyledTableCell align="right">Population</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.cases.map((data) => (
+            <StyledTableRow key={Object.keys(data)[0]}>
+              <StyledTableCell component="th" scope="row">
+                {Object.keys(data)[0]}
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                {data[Object.keys(data)].confirmed}
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                {data[Object.keys(data)].deaths}
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                {data[Object.keys(data)].population}
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 
-
-//   return (
-//     <div>
-//       <table>
-//         <thead>
-//           <tr style={{ backgroundColor: "#B2B2B2" }}>
-//             <th>Country</th>
-//             <th>Confirmed</th>
-//             <th>Deaths</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {props.cases.map((data) => (
-//             <tr style={{ backgroundColor: "#E9E3E6" }}>
-//               <td key={Object.keys(data)[0]}>{Object.keys(data)[0]}</td>
-//               <td>{data[Object.keys(data)].confirmed}</td>
-//               <td>{data[Object.keys(data)].deaths}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
+  //   return (
+  //     <div>
+  //       <table>
+  //         <thead>
+  //           <tr style={{ backgroundColor: "#B2B2B2" }}>
+  //             <th>Country</th>
+  //             <th>Confirmed</th>
+  //             <th>Deaths</th>
+  //           </tr>
+  //         </thead>
+  //         <tbody>
+  //           {props.cases.map((data) => (
+  //             <tr style={{ backgroundColor: "#E9E3E6" }}>
+  //               <td key={Object.keys(data)[0]}>{Object.keys(data)[0]}</td>
+  //               <td>{data[Object.keys(data)].confirmed}</td>
+  //               <td>{data[Object.keys(data)].deaths}</td>
+  //             </tr>
+  //           ))}
+  //         </tbody>
+  //       </table>
+  //     </div>
+  //   );
 };
 
 export default CasesList;
